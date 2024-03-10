7113ce85a81474473a201db9980ae782c8a0bae5
And Paiva <andre.pcr@hotmail.com>
2024-03-10 18:32:49 -0300

    Guessing game revisited (error handling)

---
+++ guessing_game_revisited/src/guesstype.rs
@@ -0,0 +1,20 @@
+// Developed by Andre Paiva (C) 2024
+// Rust Book-Based Personal Exercises
+
+pub struct Guess {
+    value: u8,
+}
+
+impl Guess {
+    pub fn new(value: i64) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Value must be between 1 and 100.");
+        }
+        let u8_val = value as u8;
+        Guess {value: u8_val}
+    }
+
+    pub fn value(&self) -> u8 {
+        self.value
+    }
+}
