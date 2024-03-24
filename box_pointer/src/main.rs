a57afb25d6b5e6ff8f37c64dc1e7b8e8e3237538
And Paiva <andre.pcr@hotmail.com>
2024-03-24 12:57:29 -0300

    Box (smart pointers)

--- box_pointer/src/main.rs
+++ box_pointer/src/main.rs
@@ -1,12 +1,27 @@
-#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
+impl List{
+    fn print(&self) {
+        print!("Lista:");
+        let mut item = self;
+        loop {
+            if let List::Cons(val, list) = item {
+                print!(" {}", val);
+                item = list;
+            } else {
+                println!();
+                break;
+            }
+        }
+    }
+}
+
+use crate::List::*;
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("{:?}", list);
+    list.print();
 }
