11e2a78340e4b5dacde735b98a752fb618136cb3
And Paiva <andre.pcr@hotmail.com>
2024-03-29 13:19:46 -0300

    Reject method

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -9,6 +9,25 @@ fn main() {
     post.request_review();
     assert_eq!("", post.content());
 
+    post.reject();
+    assert_eq!("", post.content());
+
+    post.add_text("\nYesterday, I ate a hamburger");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    //post.add_text("\nTomorrow, I will eat pizza");
+    //assert_eq!("", post.content());
+
     post.approve();
-    assert_eq!("I ate a salad for lunch today", post.content());
+    assert_eq!("I ate a salad for lunch today\nYesterday, I ate a hamburger",
+        post.content());
+
+    //post.add_text("\nLast weekend I ate sushi");
+    //assert_eq!("I ate a salad for lunch today\nYesterday, I ate a hamburger",
+        //post.content());
+
+    println!("{}", post.content());
 }
