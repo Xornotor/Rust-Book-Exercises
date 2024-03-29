753923e22fc9e0b46b86fd5b8a8e5c1c144dadac
And Paiva <andre.pcr@hotmail.com>
2024-03-29 13:31:17 -0300

    Impl reject function

--- blog_idiomatic/src/main.rs
+++ blog_idiomatic/src/main.rs
@@ -7,7 +7,16 @@ fn main() {
 
     let post = post.request_review();
 
+    let mut post = post.reject();
+
+    post.add_text("\nYesterday, I ate hamburger");
+
+    let post = post.request_review();
+
     let post = post.approve();
 
-    assert_eq!("I ate a salad for lunch today", post.content());
+    assert_eq!("I ate a salad for lunch today\nYesterday, I ate hamburger",
+        post.content());
+
+    println!("{}", post.content());
 }
